9db2c47eeec22f041329473d735f1e67cc4210cf
Steven Torance <storance@gmail.com>
2021-01-10 02:51:00 -0500

    Fix some issues creating the bundle.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux-immutable';
--- webpack.config.js
+++ webpack.config.js
@@ -8,91 +8,97 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 });
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
-module.exports = {
-    entry: [
-        './src/index.jsx'
-    ],
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: require.resolve('babel-loader'),
-                options: {
-                    plugins: [
-                        require.resolve('@babel/plugin-proposal-export-default-from'),
-                        isDevelopment && require.resolve('react-refresh/babel')
-                    ].filter(Boolean),
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode !== 'production'
+    let config = {
+        entry: [
+            './src/index.jsx'
+        ],
+        module: {
+            rules: [{
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        plugins: [
+                            require.resolve('@babel/plugin-proposal-export-default-from'),
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean),
+                    }
                 }
-            }
+            },
+            { 
+                test: /\.css$/, 
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            { 
+                test: /\.png$/, 
+                loader: "url-loader",
+                options: {
+                    limit: 100000
+                }
+            },
+            { 
+                test: /\.jpg$/, 
+                loader: "file-loader" 
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "application/octet-stream"
+                }
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    mimetype: "image/svg+xml"
+                }
+            }]
         },
-        { 
-            test: /\.css$/, 
-            use: [
-                "style-loader",
-                "css-loader"
-            ]
+        mode: argv.mode,
+        resolve: {
+            extensions: ['.js', '.jsx']
         },
-        { 
-            test: /\.png$/, 
-            loader: "url-loader",
-            options: {
-                limit: 100000
-            }
+        output: {
+            path: path.resolve('dist'),
+            publicPath: '/',
+            filename: 'bundle.js'
         },
-        { 
-            test: /\.jpg$/, 
-            loader: "file-loader" 
+        devServer: {
+            contentBase: './dist',
+            hot: true,
+            compress: true
         },
-        {
-            test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                mimetype: "application/font-woff"
-            }
-        },
-        {
-            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                mimetype: "application/octet-stream"
-            }
-        },
-        {
-            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-            loader: 'file-loader'
-        },
-        {
-            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-            loader: 'url-loader',
-            options: {
-                limit: 10000,
-                mimetype: "image/svg+xml"
-            }
-        }]
-    },
-    mode: isDevelopment ? 'development' : 'production',
-    devtool: 'inline-source-map',
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    output: {
-        path: path.resolve('dist'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        compress: true
-    },
-    plugins: [
-        HtmlWebpackPluginConfig,
-        isDevelopment && new webpack.HotModuleReplacementPlugin(),
-        isDevelopment && new ReactRefreshWebpackPlugin()
-    ].filter(Boolean)
+        plugins: [
+            HtmlWebpackPluginConfig,
+            isDevelopment && new webpack.HotModuleReplacementPlugin(),
+            isDevelopment && new ReactRefreshWebpackPlugin()
+        ].filter(Boolean)
+    };
+
+    if (isDevelopment) {
+        config.devtool = 'inline-source-map';
+    }
+
+    return config;
 };
