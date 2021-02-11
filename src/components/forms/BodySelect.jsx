636fac64c53547457090583f537f0369c5e94c9e
Steven Torance <storance@gmail.com>
2021-02-11 18:50:01 -0500

    Body search enhancements
    Add custom filter for the body select.  Will now filter on the body or parent body name.

--- src/components/forms/BodySelect.jsx
+++ src/components/forms/BodySelect.jsx
@@ -8,6 +8,7 @@ export default class BodySelect extends React.PureComponent {
         return <SelectField name={name}
                             label="Body"
                             searchable
+                            customFilter={this.customFilter}
                             value={this.props.body}
                             update={this.props.update}
                             options={this.getOptions()} />;
@@ -17,10 +18,20 @@ export default class BodySelect extends React.PureComponent {
         return this.appendOptions([], this.props.planetpack.sun);
     }
 
+    customFilter(candidate, input) {
+        const inputNormalized = input.toLowerCase();
+        const bodyName = candidate.data.name;
+        const parentBodyName = candidate.data.parentBody || '';
+
+        return bodyName.startsWith(inputNormalized) || parentBodyName.startsWith(inputNormalized);
+    }
+
     appendOptions(options, body, indent=0) {
         options.push({
             value: body.name,
-            label: this.buildIndent(indent) + body.name
+            label: this.buildIndent(indent) + body.name,
+            name: body.name.toLowerCase(),
+            parentBody: body.orbit ? body.orbit.parentBody.name.toLowerCase() : null
         });
 
         body.satellites.forEach(satellite => this.appendOptions(options, satellite, indent + 2));
